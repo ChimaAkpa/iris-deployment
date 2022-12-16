c9023066c63aa8cb459a7256f674da64187bc32e
ChimaAkpa <120396794+ChimaAkpa@users.noreply.github.com>
2022-12-16 16:41:24 +0100

    Add files via upload

---
+++ deployment.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 16 15:28:40 2022
+
+@author: Chima
+"""
+import numpy as np
+import pickle
+import streamlit as st
+
+load_model = pickle.load(open("C:/Users/Chima/Documents/Python codes/TEST/trained_model.sav", "rb"))
+def predictionModel(input_data, model):
+    #convert thr input data to an array using numpy
+    input_data = np.asarray(input_data)
+    # reshape the data
+    input_data_reshape = input_data.reshape(1, -1)
+    prediction = model.predict(input_data_reshape)
+    
+    return prediction
+
+def main():
+    st.title("IRISH DEPLOYMENT")
+    SepalLengthCm = st.text_input("Sepal Length Cm")
+    SepalWidthCm = st.text_input("Sepal Width Cm")
+    PetalLengthCm = st.text_input("Petal Length Cm")
+    PetalWidthCm = st.text_input("Petal Width Cm")
+    
+    features = [SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm]
+    
+    btn = st.button("Run The Model")
+     
+    if btn:
+      prediction = predictionModel(features, load_model)
+
+      if prediction[0] == 0:
+          st.write("Irish Setosa")
+      elif prediction[0] == 1:
+          st.write("Irish Versicolor")
+      else:
+          st.write("Irish Virginica")
+  
+    
+            
+if __name__ == "__main__":
+    main()
